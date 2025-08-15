f04c47f93f0834419d96222febf73756e2b9cfe7
adamzatar <azaatar@bowdoin.edu>
2025-08-15 15:32:41 -0500

    Initial Commit

---
+++ Cutaway/CutawayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CutawayApp.swift
+//  Cutaway
+//
+//  Created by Adam Zaatar on 8/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CutawayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
